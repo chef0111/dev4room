69895153323e7f5c9fd412c21588423f99c42b79
chef0111 <23520111@gm.uit.edu.vn>
2025-10-01 19:24:45 +0700

    feat: home page loading

---
+++ src/components/skeletons/PostCardsSkeleton.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@/lib/utils";
+import { Skeleton } from "../ui/skeleton";
+
+const PostCardsSkeleton = ({ className }: { className?: string }) => {
+  return (
+    <div className={cn("flex flex-col w-full gap-6", className)}>
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
+        <Skeleton key={item} className="h-44 w-full rounded-2xl" />
+      ))}
+    </div>
+  );
+};
+
+export default PostCardsSkeleton;
