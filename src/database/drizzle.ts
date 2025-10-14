d47a750bfa1d59f495d26158984f02c8d42fc6d7
chef0111 <23520111@gm.uit.edu.vn>
2025-10-14 09:53:30 +0700

    refactor: db config

--- src/database/drizzle.ts
+++ src/database/drizzle.ts
@@ -4,4 +4,9 @@ import * as schema from "./schema";
 
 config({ path: ".env" }); // or .env.local
 
-export const db = drizzle(process.env.DATABASE_URL!, { schema });
+const url = process.env.DATABASE_URL;
+if (!url) {
+  throw new Error("DATABASE_URL is not set");
+}
+
+export const db = drizzle(url, { schema });
