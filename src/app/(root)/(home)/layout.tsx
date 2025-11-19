204601537203bbcad59fffdb9ee978076728797a
chef0111 <23520111@gm.uit.edu.vn>
2025-11-19 15:12:16 +0700

    refactor: optimize layout

--- src/app/(root)/(home)/layout.tsx
+++ src/app/(root)/(home)/layout.tsx
@@ -1,5 +1,3 @@
-import Navbar from "@/components/layout/navigation/navbar";
-import LeftSidebar from "@/components/layout/navigation/LeftSidebar";
 import RightSidebar from "@/components/layout/navigation/RightSidebar";
 
 interface Props {
@@ -8,19 +6,10 @@ interface Props {
 
 const HomeLayout = ({ children }: Props) => {
   return (
-    <main className="bg-light850_dark100 relative">
-      <Navbar />
-
-      <div className="block">
-        <LeftSidebar />
-
-        <section className="flex min-h-screen flex-1 flex-col min-w-full px-6 pb-6 pt-32 max-md:pb-14 sm:px-14 sm:pl-36 lg:pl-80 xl:pr-88">
-          <div className="mx-auto w-full">{children}</div>
-        </section>
-
-        <RightSidebar />
-      </div>
-    </main>
+    <>
+      <div className="xl:pr-74">{children}</div>
+      <RightSidebar />
+    </>
   );
 };
 
--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,3 +1,5 @@
+import LeftSidebar from "@/components/layout/navigation/LeftSidebar";
+import Navbar from "@/components/layout/navigation/navbar";
 import NextTopLoader from "nextjs-toploader";
 interface Props {
   children: React.ReactNode;
@@ -5,10 +7,18 @@ interface Props {
 
 const DashboardLayout: React.FC<Props> = ({ children }) => {
   return (
-    <>
+    <main className="bg-light850_dark100 relative">
       <NextTopLoader showSpinner={false} />
-      {children}
-    </>
+      <Navbar />
+
+      <div className="block">
+        <LeftSidebar />
+
+        <section className="flex min-h-screen flex-1 flex-col min-w-full px-6 pb-6 pt-32 max-md:pb-14 sm:px-14 sm:pl-34 lg:pl-78 2xl:px-0">
+          <div className="mx-auto w-full">{children}</div>
+        </section>
+      </div>
+    </main>
   );
 };
 
