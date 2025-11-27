e47784a9abf358317043fb5708873540103a1f63
chef0111 <23520111@gm.uit.edu.vn>
2025-11-27 16:24:28 +0700

    feat: implement orpc on community page

--- src/app/(root)/(home)/community/page.tsx
+++ src/app/(root)/(home)/community/page.tsx
@@ -1,47 +1,57 @@
-import { fetchUsers } from "@/server/user/user.action";
 import { EMPTY_USERS } from "@/common/constants/states";
 import LocalSearch from "@/components/layout/main/LocalSearch";
 import UserCard from "@/components/layout/profile/UserCard";
 import DataRenderer from "@/components/shared/DataRenderer";
+import { orpc } from "@/lib/orpc";
+import { getQueryClient, HydrateClient } from "@/lib/query/hydration";
 
 const Community = async ({ searchParams }: RouteParams) => {
   const { page, pageSize, query, filter } = await searchParams;
 
-  const { success, data, error } = await fetchUsers({
-    page: Number(page) || 1,
-    pageSize: Number(pageSize) || 12,
-    query,
-    filter,
+  const queryClient = getQueryClient();
+
+  const queryOptions = orpc.user.list.queryOptions({
+    input: {
+      page: Number(page) || 1,
+      pageSize: Number(pageSize) || 12,
+      query,
+      filter,
+    },
   });
 
-  const { users } = data || {};
+  await queryClient.prefetchQuery(queryOptions);
+
+  const data = queryClient.getQueryData(queryOptions.queryKey);
+  const users = data?.users;
 
   return (
-    <div>
-      <h1 className="h1-bold text-dark100_light900">All Users</h1>
+    <HydrateClient client={queryClient}>
+      <div>
+        <h1 className="h1-bold text-dark100_light900">All Users</h1>
 
-      <section className="mt-10">
-        <LocalSearch
-          route="/community"
-          placeholder="Search some great developers..."
-          className="flex-1"
+        <section className="mt-10">
+          <LocalSearch
+            route="/community"
+            placeholder="Search some great developers..."
+            className="flex-1"
+          />
+        </section>
+
+        <DataRenderer
+          success={!!data}
+          error={data ? undefined : { message: "Failed to fetch users" }}
+          data={users}
+          empty={EMPTY_USERS}
+          render={(users) => (
+            <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 w-full gap-4">
+              {users.map((user) => (
+                <UserCard key={user.id} {...user} />
+              ))}
+            </div>
+          )}
         />
-      </section>
-
-      <DataRenderer
-        success={success}
-        error={error}
-        data={users}
-        empty={EMPTY_USERS}
-        render={(users) => (
-          <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 w-full gap-4">
-            {users.map((user) => (
-              <UserCard key={user.id} {...user} />
-            ))}
-          </div>
-        )}
-      />
-    </div>
+      </div>
+    </HydrateClient>
   );
 };
 
