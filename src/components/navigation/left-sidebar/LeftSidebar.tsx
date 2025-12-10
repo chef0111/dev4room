f435ba4836375e810a4110f832dbb9b64500d6bf
chef0111 <letrangiabao14@gmail.com>
2025-12-10 08:34:43 +0700

    perf: optimize nav rendering

--- src/components/navigation/left-sidebar/LeftSidebar.tsx
+++ src/components/navigation/left-sidebar/LeftSidebar.tsx
@@ -1,17 +1,9 @@
 import { Suspense } from "react";
-import Link from "next/link";
-import Image from "next/image";
-import { getServerSession } from "@/lib/session";
 import NavTabs from "./NavTabs";
-import { Button, Skeleton } from "@/components/ui";
-import { UserNav } from "@/components/modules/profile/UserNav";
 import NavTabsFallback from "./NavTabsFallback";
+import SidebarUser from "./SidebarUser";
 
-const LeftSidebar = async () => {
-  const session = await getServerSession();
-  const user = session?.user;
-  const isAdmin = user?.role === "admin";
-
+const LeftSidebar = () => {
   return (
     <div>
       <section
@@ -21,57 +13,11 @@ const LeftSidebar = async () => {
       >
         <div>
           <Suspense fallback={<NavTabsFallback isMobile={false} />}>
-            <NavTabs userId={user?.id} isMobile={false} />
+            <NavTabs isMobile={false} />
           </Suspense>
         </div>
 
-        <div className="flex flex-col gap-3">
-          {user ? (
-            <Suspense
-              fallback={<Skeleton className="h-10 w-full rounded-lg" />}
-            >
-              <UserNav user={user} isAdmin={isAdmin} />
-            </Suspense>
-          ) : (
-            <>
-              <Button
-                className="btn-secondary no-focus min-h-10 w-full rounded-lg px-4 py-3 hover:bg-light700_dark300! transition-all duration-200 cursor-pointer"
-                asChild
-              >
-                <Link href="/login" className="flex-center">
-                  <Image
-                    src="/icons/account.svg"
-                    alt="Account"
-                    width={20}
-                    height={20}
-                    className="invert-colors lg:hidden"
-                  />
-                  <span className="primary-text-gradient small-medium max-lg:hidden">
-                    Log in
-                  </span>
-                </Link>
-              </Button>
-
-              <Button
-                className="btn-tertiary no-focus light-border-2 min-h-10 w-full rounded-lg border px-4 py-3 hover:bg-light600_dark400! transition-all duration-200 cursor-pointer shadow-none"
-                asChild
-              >
-                <Link href="/register" className="flex-center">
-                  <Image
-                    src="/icons/sign-up.svg"
-                    alt="Sign Up"
-                    width={20}
-                    height={20}
-                    className="invert-colors lg:hidden"
-                  />
-                  <span className="text-dark400_light900 small-medium max-lg:hidden">
-                    Register
-                  </span>
-                </Link>
-              </Button>
-            </>
-          )}
-        </div>
+        <SidebarUser />
       </section>
     </div>
   );
