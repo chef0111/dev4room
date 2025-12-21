b3aef7c886bc542f8c51f109a7d483f86bac23a7
chef0111 <letrangiabao14@gmail.com>
2025-12-21 21:40:25 +0700

    chore: update table header styling

--- src/components/data-table/data-table.tsx
+++ src/components/data-table/data-table.tsx
@@ -35,7 +35,7 @@ export function DataTable<TData>({
         <Table>
           <TableHeader>
             {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
+              <TableRow key={headerGroup.id} className="bg-muted/50!">
                 {headerGroup.headers.map((header) => (
                   <TableHead
                     key={header.id}
@@ -61,6 +61,7 @@ export function DataTable<TData>({
                 <TableRow
                   key={row.id}
                   data-state={row.getIsSelected() && "selected"}
+                  className="smooth-hover"
                 >
                   {row.getVisibleCells().map((cell) => (
                     <TableCell
